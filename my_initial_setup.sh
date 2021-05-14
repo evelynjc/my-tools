355a6b650155e5372bb250756713082734e4be65
Evelyn Choe <47475637+JHJee@users.noreply.github.com>
2021-05-14 21:36:12 +0900

    Update my_initial_setup.sh

--- my_initial_setup.sh
+++ my_initial_setup.sh
@@ -5,8 +5,10 @@ sudo apt clean
 sudo apt install -y vim git python3 python3-pip httpie screen dos2unix
 git config --global user.email "chlwlgp132@gmail.com"
 git config --global user.name "JHJee"
-# for cross-platform line ending compatibility (LF for Linux & CRLF for Win)
-git config --global core.autocrlf true
+# for cross-platform line ending compatibility in Windows
+#git config --global core.autocrlf true
+# for cross-platform line ending compatibility in Linux
+#git config --global core.autocrlf input
 
 mv ./vimrc ~/.vimrc
 cp ./bash_aliases ~/.bash_aliases && source ~/.bashrc
